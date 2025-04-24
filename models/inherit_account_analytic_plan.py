6d893484d657d4085865f8df79d39772eaf45d03
Pasquale <cordisco.pasquale@yahoo.com>
2025-04-24 17:52:49 +0200

    up

--- models/inherit_account_analytic_plan.py
+++ models/inherit_account_analytic_plan.py
@@ -3,4 +3,8 @@ from odoo import fields, models
 class InheritAccountAnalyticPlan(models.Model):
     _inherit = 'account.analytic.plan'
 
+    _sql_constraints = [
+        ('account_analytic_plan_constraints', 'unique(name, company_id)', 'The plan name must be unique per company!')
+    ]
+
     company_id = fields.Many2one('res.company', string='Company',  default=lambda self: self.env.company,)
